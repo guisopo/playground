d2e30734828e724455f46086ce1d031cf9d6b8f9
guisopo <solerguillermo@hotmail.com>
2019-01-24 09:44:51 +0100

    Created new pixi app for each section

---
+++ pixi/bookwave/main.js
@@ -0,0 +1,30 @@
+const sections = document.querySelectorAll('section');
+
+sections.forEach(section => {
+  const originalImg = section.querySelector('img');
+  const originalImgSrc = originalImg.getAttribute('src');
+
+  section.innerHTML = '';
+
+  //Set up pixi application
+  const app = new PIXI.Application({
+    width: 1100,
+    height: 800,
+    transparent: true
+  });
+
+  //Add pixi app to each section
+  section.appendChild(app.view);
+
+  //Make new loader
+  const loader = new PIXI.loaders.Loader();
+
+  //Load images
+  loader.add('image', originalImgSrc);
+  loader.load((loader, resources) => {
+    //Once image loaded do this
+    const image = new PIXI.Sprite(resources.image.texture);
+    //Add image to app
+    app.stage.addChild(image);
+  })
+});
