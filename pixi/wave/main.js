9201414e71583b61d65e8f68f2ca796fbfa1997e
guisopo <solerguillermo@hotmail.com>
2019-01-26 23:08:15 +0100

    Transfered methods to the class

--- pixi/wave/main.js
+++ pixi/wave/main.js
@@ -4,19 +4,54 @@ class ImageLoad {
     this.width = wrapper.getBoundingClientRect().width;
     this.height = wrapper.getBoundingClientRect().height;
 
-    this.app = new PIXI.Application(this.width, this.height, {backgroundColor: 0x000000});    
+    this.app = new PIXI.Application(this.width, this.height, {transparent: true});    
     this.wrapper.appendChild(this.app.view);
+    this.src = wrapper.dataset.src;
 
     this.container = new PIXI.Container();
     this.app.stage.addChild(this.container);
+
+    this.load(this.startAnimation());
   }
 
   load(afterLoad) {
-
+    let tmpImg = new Image();
+    tmpImg.src = this.src;
+    tmpImg.onLoad = function() {
+      afterLoad();
+    }
   }
 
   startAnimation() {
+    this.bg = PIXI.Sprite.fromImage(this.src);
+    this.bg.width = this.width;
+    this.bg.height = this.height;
+    this.bg.position.x =  0;
+    this.bg.position.y =  0;
+    this.container.addChild(this.bg);
 
+    this.displacementSprite = PIXI.Sprite.fromImage('assets/displacement9.jpg');
+    this.displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+
+    this.displacementFilter = new PIXI.filters.DisplacementFilter(
+      this.displacementSprite,
+      1e4 + Math.random() * 1e3
+    );
+
+    this.displacementSprite.scale.set(0.4 + 0.6 * Math.random());
+
+    this.app.stage.addChild(this.displacementSprite);
+
+    this.container.filters = [this.displacementFilter];
+    this.click();
+
+  }
+
+  click() {
+    this.wrapper.addEventListener('click', () => {
+      let tl = new TimelineMax();
+      tl.to(this.displacementFilter.scale, 1, {x: 1, y: 1});
+    });
   }
 
   hover() {
