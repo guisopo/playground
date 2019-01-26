a1c97440ffb4a5c28f42a7eeea23fddd957f6b16
guisopo <solerguillermo@hotmail.com>
2019-01-27 00:01:56 +0100

    Removed click method and commented code

--- pixi/wave/main.js
+++ pixi/wave/main.js
@@ -45,15 +45,10 @@ class ImageLoad {
     this.app.stage.addChild(this.displacementSprite);
 
     this.container.filters = [this.displacementFilter];
-    this.click();
-    this.hover();
-  }
 
-  click() {
-    this.wrapper.addEventListener('click', () => {
-      let tl = new TimelineMax({onComplete:() => this.animated = true});
-      tl.to(this.displacementFilter.scale, 1, {x: 1, y: 1});
-    });
+    let tl = new TimelineMax({onComplete:() => this.animated = true});
+    tl.to(this.displacementFilter.scale, 1, {x: 1, y: 1});
+    this.hover();
   }
 
   hover() {
@@ -82,36 +77,7 @@ class ImageLoad {
 
 const loadImages = document.querySelectorAll('.js-loadme');
 
-loadImages.forEach((image, el) => {
+loadImages.forEach((image) => {
   let img = new ImageLoad(image);
 })
 
-
-// const app = new PIXI.Application(300, 300, {backgroundColor: 0x000000});
-
-// document.body.appendChild(app.view);
-
-// let container = new PIXI.Container();
-// app.stage.addChild(container);
-
-// let bg = PIXI.Sprite.fromImage('./assets/book0.png');
-// bg.width = 300;
-// bg.height = 300;
-// bg.position.x =  0;
-// bg.position.y =  0;
-// container.addChild(bg);
-
-// let displacementSprite = PIXI.Sprite.fromImage('assets/displacement3.jpg');
-// let displacementFilter = new PIXI.filters.DisplacementFilter(
-//   displacementSprite,
-//   1e2
-// );
-
-// app.stage.addChild(displacementSprite);
-
-// container.filters = [displacementFilter];
-
-// document.body.addEventListener('click',  function() {
-//   let tl = new TimelineMax();
-//   tl.to(displacementFilter.scale, 1, {x: 1, y: 1});
-// });
