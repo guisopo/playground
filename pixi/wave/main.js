d5d81e0fc0a261a7d833a1342372d18d44389740
guisopo <solerguillermo@hotmail.com>
2019-01-26 21:26:51 +0100

    Created new class to handle the process and DIV with data-sources

--- pixi/wave/main.js
+++ pixi/wave/main.js
@@ -1,28 +1,61 @@
-const app = new PIXI.Application(300, 300, {backgroundColor: 0x000000});
+class ImageLoad {
+  constructor(wrapper) {
+    this.wrapper = wrapper;
+    this.width = wrapper.getBoundingClientRect().width;
+    this.height = wrapper.getBoundingClientRect().height;
 
-document.body.appendChild(app.view);
+    this.app = new PIXI.Application(this.width, this.height, {backgroundColor: 0x000000});    
+    this.wrapper.appendChild(this.app.view);
 
-let container = new PIXI.Container();
-app.stage.addChild(container);
+    this.container = new PIXI.Container();
+    this.app.stage.addChild(this.container);
+  }
 
-let bg = PIXI.Sprite.fromImage('./assets/book0.png');
-bg.width = 300;
-bg.height = 300;
-bg.position.x =  0;
-bg.position.y =  0;
-container.addChild(bg);
+  load(afterLoad) {
 
-let displacementSprite = PIXI.Sprite.fromImage('assets/displacement3.jpg');
-let displacementFilter = new PIXI.filters.DisplacementFilter(
-  displacementSprite,
-  10
-);
+  }
 
-app.stage.addChild(displacementSprite);
+  startAnimation() {
 
-container.filters = [displacementFilter];
+  }
 
-document.body.addEventListener('click',  function() {
-  let tl = new TimelineMax();
-  tl.to(displacementFilter.scale, 1, {x: 1000, y: 1000});
-});
+  hover() {
+
+  }
+}
+
+const loadImages = document.querySelectorAll('.js-loadme');
+
+loadImages.forEach((image, el) => {
+  let img = new ImageLoad(image);
+})
+
+
+// const app = new PIXI.Application(300, 300, {backgroundColor: 0x000000});
+
+// document.body.appendChild(app.view);
+
+// let container = new PIXI.Container();
+// app.stage.addChild(container);
+
+// let bg = PIXI.Sprite.fromImage('./assets/book0.png');
+// bg.width = 300;
+// bg.height = 300;
+// bg.position.x =  0;
+// bg.position.y =  0;
+// container.addChild(bg);
+
+// let displacementSprite = PIXI.Sprite.fromImage('assets/displacement3.jpg');
+// let displacementFilter = new PIXI.filters.DisplacementFilter(
+//   displacementSprite,
+//   1e2
+// );
+
+// app.stage.addChild(displacementSprite);
+
+// container.filters = [displacementFilter];
+
+// document.body.addEventListener('click',  function() {
+//   let tl = new TimelineMax();
+//   tl.to(displacementFilter.scale, 1, {x: 1, y: 1});
+// });
