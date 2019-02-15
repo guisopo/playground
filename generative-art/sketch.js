696e12aca4d6c2543c8a7dedb4d044f6a863e8be
guisopo <solerguillermo@hotmail.com>
2019-02-15 19:01:11 +0100

    Created grid

--- generative-art/sketch.js
+++ generative-art/sketch.js
@@ -5,9 +5,34 @@ const settings = {
 };
 
 const sketch = () => {
+  const createGrid = (n) => {
+    const points = [];
+    const count = n;
+    for(let x = 0; x < count; x++) {
+      for(let y = 0; y < count; y++) {
+        const u = (count <= 1) ? 0.5 : x/(count - 1);
+        const v = (count <= 1) ? 0.5 : y/(count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  const points = createGrid(4);
+  console.log(points);
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    points.forEach(([u, v]) => {
+      const x = u * width;
+      const y = v * height;
+
+      context.beginPath();
+      context.arc(x, y, 20, Math.PI * 2, false);
+      context.fillStyle= 'black';
+      context.fill();
+    });
   };
 };
 
