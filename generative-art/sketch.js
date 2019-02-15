aae99bf3af88453a9bef4c3b77d5da1890650a26
guisopo <solerguillermo@hotmail.com>
2019-02-15 20:59:22 +0100

    Added random values to generate the circles

--- generative-art/sketch.js
+++ generative-art/sketch.js
@@ -1,11 +1,15 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
 
 const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(2, 6);
+  const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
   const createGrid = (n) => {
     const points = [];
     const count = n;
@@ -13,25 +17,36 @@ const sketch = () => {
       for(let y = 0; y < count; y++) {
         const u = (count <= 1) ? 0.5 : x/(count - 1);
         const v = (count <= 1) ? 0.5 : y/(count - 1);
-        points.push([u, v]);
+        points.push({
+          radius: Math.abs(random.gaussian() * 0.01),
+          position: [u, v],
+          color: random.pick(palette)
+        });
       }
     }
     return points;
   };
 
-  const points = createGrid(4);
-  const margin = 500;
+  random.setSeed(512);
+  const points = createGrid(40).filter( () => random.value() > 0.5);
+  const margin = 300;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-    points.forEach(([u, v]) => {
+    points.forEach( point => {
+      const {
+        position,
+        radius,
+        color
+      } = point;
+      const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, width - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 20, Math.PI * 2, false);
-      context.fillStyle= 'black';
+      context.arc(x, y, radius * width, Math.PI * 2, false);
+      context.fillStyle= color;
       context.fill();
     });
   };
