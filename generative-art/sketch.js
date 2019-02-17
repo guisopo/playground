826885c1407b4a1b10278deaee02b5841056d7a9
guisopo <solerguillermo@hotmail.com>
2019-02-17 14:37:54 +0100

    Added rotation to the particles

--- generative-art/sketch.js
+++ generative-art/sketch.js
@@ -17,12 +17,13 @@ const sketch = () => {
       for(let y = 0; y < count; y++) {
         const u = (count <= 1) ? 0.5 : x/(count - 1);
         const v = (count <= 1) ? 0.5 : y/(count - 1);
-        const radius = Math.abs(random.noise2D(u, v)) * 0.01;
+        const radius = Math.abs(random.noise2D(u, v)) * 0.1;
         points.push({
           // radius: Math.abs(random.gaussian() * 0.01),
           radius: radius,
           position: [u, v],
-          color: random.pick(palette)
+          color: random.pick(palette),
+          rotation : Math.abs(random.noise2D(u, v))
         });
       }
     }
@@ -30,7 +31,7 @@ const sketch = () => {
   };
 
   // random.setSeed(512);
-  const points = createGrid(40).filter( () => random.value() > 0.5);
+  const points = createGrid(50).filter( () => random.value() > 0.5);
   const margin = 300;
 
   return ({ context, width, height }) => {
@@ -40,16 +41,26 @@ const sketch = () => {
       const {
         position,
         radius,
-        color
+        color,
+        rotation
       } = point;
       const [u, v] = position;
       const x = lerp(margin, width - margin, u);
-      const y = lerp(margin, width - margin, v);
+      const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, Math.PI * 2, false);
-      context.fillStyle= color;
-      context.fill();
+      // context.beginPath();
+      // context.arc(x, y, radius * width, Math.PI * 2, false);
+      // context.fillStyle= color;
+      // context.fill();
+      context.save();
+
+      context.fillStyle = color;
+      context.font= `${radius * width}px "Helvetica"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('-', 0, 0);
+
+      context.restore();
     });
   };
 };
