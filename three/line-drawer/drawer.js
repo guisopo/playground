9ec3d743a31fe96e2b6781ca69a45ffcf1267cac
guisopo <solerguillermo@hotmail.com>
2019-02-19 10:29:43 +0100

    Created lighting, shapes array that holds each shape info and mouse events

--- three/line-drawer/drawer.js
+++ three/line-drawer/drawer.js
@@ -19,26 +19,62 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.z = -50;
 camera.lookAt(scene.position);
 
-// 4. Add the animation loop
+// 4. Add Lighting
+const light = new THREE.DirectionalLight(0xffffff, 1);
+// set Light Source to come from the User view point
+light.position.set(0, 0, -1);
+scene.add(light);
+
+// 5. Hold some data from the Shape being added
+const shapes = [];
+
+// 6. Add the animation loop
 const animate = function() {
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
+
+  //Rotate shapes each frame
+  shapes.forEach(shape => {
+    shape.rotateX(0.01);
+    shape.rotateZ(0.005);
+  });
 }
 
 animate();
 
-// 5. Create shapes
-const createShape = function() {
+// 7. Create Shapes
+const createShape = function(x, y, z) {
   const geometry = new THREE.ConeGeometry(10, 10, 32);
   const material = new THREE.MeshLambertMaterial({
-    color: 0xff0000,
+    color: 0xffffff,
+    emissive: 0xff0000,
   });
   const shape = new THREE.Mesh(geometry, material);
 
-  shape.rotateX(-0.5);
-  shape.rotateZ(0.5);
+  //Set Shape's Position
+  shape.position.set( (window.innerWidth / 2) - x, (window.innerHeight / 2) - y, z);
+
+  // Lets add it to the Shapes Array and to the Scene
+  shapes.push(shape);
 
   scene.add(shape);
 }
 
-createShape();
+
+// 8. Mouse Events
+let isMouseDown = false;
+
+document.addEventListener('mousemove', function(event) {
+  if(isMouseDown) {
+    createShape(event.pageX, event.pageY, 300);
+  }
+});
+
+document.addEventListener('mousedown', function() {
+  isMouseDown = true;
+  console.log(isMouseDown);
+});
+
+document.addEventListener('mouseup', function() {
+  isMouseDown = false;
+});
