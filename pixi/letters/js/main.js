c411521773208bc2077a61fc86e93340a54aa565
guisopo <solerguillermo@hotmail.com>
2019-03-20 19:14:18 +0100

    Added basic pixi setup

--- pixi/letters/js/main.js
+++ pixi/letters/js/main.js
@@ -1,3 +1,19 @@
+//Set up pixi application
+const app = new PIXI.Application({transparent: true});
+//Add pixi app to html
+document.body.appendChild(app.view);
+
+let container = new PIXI.Container();
+
+app.stage.addChild(container);
+
+function init() {
+  let count = 0
 
 
-console.log('hola');
+  app.ticker.add(function() {
+    count++;
+  });
+}
+
+init();
