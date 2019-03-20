41f19f374edd548b8e2b35690d6baebec44d53e1
guisopo <solerguillermo@hotmail.com>
2019-03-20 19:25:41 +0100

    Added text in the pixi container with google webfont loader

--- pixi/letters/js/main.js
+++ pixi/letters/js/main.js
@@ -1,19 +1,36 @@
+WebFont.load({
+  google: {
+    families: ['Francois One']
+  },
+  active: function() {
+    init();
+  }
+});
+
 //Set up pixi application
 const app = new PIXI.Application({transparent: true});
 //Add pixi app to html
 document.body.appendChild(app.view);
 
-let container = new PIXI.Container();
+const container = new PIXI.Container();
 
 app.stage.addChild(container);
 
 function init() {
   let count = 0
 
+  const textArguments = {
+    fontFamily : 'Francois One', 
+    fontSize: 24, 
+    fill : 0x000000, 
+    align : 'center'
+  };
+
+  const text = new PIXI.Text('This is a PixiJS text', textArguments);
+
+  container.addChild(text);
 
   app.ticker.add(function() {
     count++;
   });
 }
-
-init();
