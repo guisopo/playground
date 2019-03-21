32c3cf9a9f2834e04dad0114f5971c8378a2b996
guisopo <solerguillermo@hotmail.com>
2019-03-21 19:45:14 +0100

    Added first line

--- three/lines-pic/src/index.js
+++ three/lines-pic/src/index.js
@@ -20,6 +20,23 @@ const scene = new THREE.Scene();
 const group = new THREE.Group();
 scene.add(group);
 
+// LINE INIT
+const material = new THREE.LineBasicMaterial( {
+   color: 0xff0000
+} );
+const geometry = new THREE.Geometry();
+
+const line = new THREE.Line(geometry, material);
+
+const vector1 = new THREE.Vector3(-100, 0, 0);
+const vector2 = new THREE.Vector3(100, 0, 0);
+
+geometry.vertices.push(vector1);
+geometry.vertices.push(vector2);
+
+line.rotation.z = Math.random() *Math.PI;
+group.add(line);
+
 // CAMERA
 const camera = new THREE.PerspectiveCamera(40, width/height, 1, 1000);
 camera.position.set(0, 0, 300);
