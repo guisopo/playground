14756ba82820a2359803d27a3d9b4d91eb01a459
guisopo <solerguillermo@hotmail.com>
2019-03-21 19:44:55 +0100

    Initial three.js setup

--- three/lines-pic/src/index.js
+++ three/lines-pic/src/index.js
@@ -1,3 +1,27 @@
-import { consoleName } from "./utils/console";
 import "./main.scss";
-consoleName('Guillermos');
+import * as THREE from 'three';
+
+const canvas = document.getElementById('myscene');
+const width = 600;
+const height = 600;
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true
+});
+
+renderer.setPixelRatio(window.devicePixelRatio>1?2:1);
+renderer.setSize(width, height);
+renderer.setClearColor(0x999999);
+
+// SCENE
+const scene = new THREE.Scene();
+const group = new THREE.Group();
+scene.add(group);
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(40, width/height, 1, 1000);
+camera.position.set(0, 0, 300);
+
+renderer.render(scene, camera);
