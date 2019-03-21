327cf45d311ff8746bec00194eba3c807e773b2e
guisopo <solerguillermo@hotmail.com>
2019-03-21 20:03:55 +0100

    Added for loop that adds 50 lines with random rotations in 3 directions

--- three/lines-pic/src/index.js
+++ three/lines-pic/src/index.js
@@ -27,21 +27,36 @@ scene.add(group);
 const material = new THREE.LineBasicMaterial( {
    color: 0xff0000
 } );
-const geometry = new THREE.Geometry();
-
-const line = new THREE.Line(geometry, material);
 
 const vector1 = new THREE.Vector3(-100, 0, 0);
 const vector2 = new THREE.Vector3(100, 0, 0);
 
-geometry.vertices.push(vector1);
-geometry.vertices.push(vector2);
+const geometry = new THREE.Geometry();
 
-line.rotation.z = Math.random() *Math.PI;
-group.add(line);
+for(let i=0; i<lines; i++) {
+  const line = new THREE.Line(geometry, material);
+
+  geometry.vertices.push(vector1);
+  geometry.vertices.push(vector2);
+
+  line.rotation.x = Math.random() *Math.PI;
+  line.rotation.y = Math.random() *Math.PI;
+  line.rotation.z = Math.random() *Math.PI;
+
+  group.add(line);
+}
 
 // CAMERA
 const camera = new THREE.PerspectiveCamera(40, width/height, 1, 1000);
 camera.position.set(0, 0, 300);
 
-renderer.render(scene, camera);
+// ORBIT CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// RENDER FUNCTION
+function render() {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(render);
+}
+
+render();
