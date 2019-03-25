03a7e942036317aacecfe2d7469b03ae1dbd3e22
guisopo <solerguillermo@hotmail.com>
2019-03-25 16:51:11 +0100

    Addded new timelinemax

--- three/sliderGL/src/index.js
+++ three/sliderGL/src/index.js
@@ -1,7 +1,23 @@
 import "./main.scss";
+import { TimelineMax } from 'gsap';
 
-const slider = document.querySelector('.slider__wrap');
+let state = {pos:0};
 
 document.body.addEventListener('click', () => {
-  slider.classList.add('a');
+  let tl = new TimelineMax();
+  let tl1 = new TimelineMax();
+  tl
+    .to(state, 2, {
+      pos:1,
+      onUpdate: function() {
+        let percentage = `-${state.pos * 100}%`;
+        let percentage1 = `-${state.pos*state.pos * 100}%`;
+        let percentage2 = `-${state.pos*state.pos*state.pos * 100}%`;
+
+        tl1
+          .set('.slider__1 .slider__wrap', {x: percentage}, 0)
+          .set('.slider__2 .slider__wrap', {x: percentage1}, 0)
+          .set('.slider__3 .slider__wrap', {x: percentage2}, 0);
+      }
+    })
 });
