8ff2f2e7efc726b3faab252e6bdf18b94d8675ce
guisopo <solerguillermo@hotmail.com>
2019-03-25 10:07:31 +0100

    Added path arrays and moved init function insidea an image loader function

--- three/svgThree/src/index.js
+++ three/svgThree/src/index.js
@@ -11,101 +11,130 @@ canvas.width = size;
 canvas.height = size;
 canvas.classList.add('temp-canvas');
 document.body.appendChild(canvas);
-let imageCoords = [];
 
-const img = new Image();
-img.onload = function() {
+const paths = [
+  'src/images/arrow.svg',
+  'src/images/close.svg',
+  'src/images/place.svg',
+];
+
+function loadImages(paths, whenLoaded) {
+  let imgs = [];
+  paths.forEach(path => {
+    const img = new Image;
+    img.onload = function() {
+      imgs.push(img);
+      if(imgs.length==paths.length) whenLoaded(imgs);
+    }
+    img.src = path;
+  });
+}
+
+function getAlphaFromImage(img) { 
+  let imageCoords = [];
+  context.clearRect(0, 0, size, size);
   context.drawImage(img, 0, 0, size, size);
   let data = context.getImageData(0, 0, size, size);
   data = data.data;
-
-  for(let y=0; y<=size; y++) {
-    for(let x=0; x<=size; x++) {
+  
+  for(let y=0; y<size; y++) {
+    for(let x=0; x<size; x++) {
       let alpha = data[((size * y) + x) * 4 + 3];
       if(alpha > 0) {
         imageCoords.push([10*(x - size/2), 10*(y - size/2)]);
       }
     }
   }
-
-}
-console.log(imageCoords);
-img.src= 'src/images/close.svg';
-
-
-let camera, controls, scene, renderer, geometry;
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-function init() {
-  // SCENE
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0x000000  );
-  
-  // RENDERER
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
-  renderer.setSize(width, height);
-
-  const container = document.getElementById('container');
-  container.appendChild(renderer.domElement);
-  
-  // CAMERA
-  camera = new THREE.PerspectiveCamera(90, width/height, 1, 1000);
-  camera.position.z = 500;
-  
-  // ORBIT CONTROLS
-  controls = new OrbitControls(camera, renderer.domElement);
-  
-  // TEXTURE
-  const texture = (new THREE.TextureLoader).load('src/images/particle.png');
-  const material = new THREE.PointsMaterial({
-    size: 10,
-    vertexColors: THREE.VertexColors,
-    map: texture,
-    alphaTest: 0.5
-  });
-  
-  // GEOMETRY
-  geometry = new THREE.Geometry();
-  let x, y, z;
-  
-  for(let i=0; i<=100000; i++) {
-    x = Math.sin(i/10) * 100;
-    y = Math.cos(i/10) * 100;
-    z = i * 1;
-  
-    geometry.vertices.push(new THREE.Vector3(x, y, z));
-    geometry.colors.push(new THREE.Color(Math.random(), Math.random(), Math.random()));
-  };
-
-  const pointMaterial = new THREE.Points(geometry, material);
-  scene.add(pointMaterial);
+  return imageCoords;
 }
 
-// ANIMATION
-let i = 0;
+loadImages(paths, function(loadedImages) {
+  let img0 = getAlphaFromImage(loadedImages[0]);
+  let img1 = getAlphaFromImage(loadedImages[1]);
+  let img2 = getAlphaFromImage(loadedImages[2]);
 
-function animate() {
-  i++;
-  requestAnimationFrame(animate);
 
-  geometry.vertices.forEach((vector, index) => {
-    let dX, dY, dZ;
-    // dX = Math.sin(i/10 + index/2) * 2;
-    dY = 0;
-    dZ = 0;
-    vector.add(new THREE.Vector3(dX, dY, dZ));
-  });
 
-  geometry.verticesNeedUpdate = true;
 
-  render();
-}
+  let camera, controls, scene, renderer, geometry;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
-function render() {
-  renderer.render(scene, camera);
-}
+  function init() {
+    // SCENE
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x000000  );
+    
+    // RENDERER
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+    renderer.setSize(width, height);
 
-init();
-animate();
+    const container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
+    
+    // CAMERA
+    camera = new THREE.PerspectiveCamera(90, width/height, 1, 1000);
+    camera.position.z = 500;
+    
+    // ORBIT CONTROLS
+    controls = new OrbitControls(camera, renderer.domElement);
+    
+    // TEXTURE
+    const texture = (new THREE.TextureLoader).load('src/images/particle.png');
+    const material = new THREE.PointsMaterial({
+      size: 10,
+      vertexColors: THREE.VertexColors,
+      map: texture,
+      alphaTest: 0.5
+    });
+    
+    // GEOMETRY
+    geometry = new THREE.Geometry();
+    // let x, y, z;
+    
+    // for(let i=0; i<=100000; i++) {
+    //   x = Math.sin(i/10) * 100;
+    //   y = Math.cos(i/10) * 100;
+    //   z = i * 1;
+    
+    //   geometry.vertices.push(new THREE.Vector3(x, y, z));
+    //   geometry.colors.push(new THREE.Color(Math.random(), Math.random(), Math.random()));
+    // };
+
+    img2.forEach((coord, index) => {
+      geometry.vertices.push(new THREE.Vector3(coord[0], coord[1], Math.random()*100));
+      geometry.colors.push(new THREE.Color(Math.random(), Math.random(), Math.random()));
+    });
+
+    const pointMaterial = new THREE.Points(geometry, material);
+    scene.add(pointMaterial);
+  }
+
+  // ANIMATION
+  let i = 0;
+
+  function animate() {
+    i++;
+    requestAnimationFrame(animate);
+
+    geometry.vertices.forEach((vector, index) => {
+      let dX, dY, dZ;
+      dX = Math.sin(i/5 + index/2) * 0.25;
+      dY = 0;
+      dZ = 0;
+      vector.add(new THREE.Vector3(dX, dY, dZ));
+    });
+
+    geometry.verticesNeedUpdate = true;
+
+    render();
+  }
+
+  function render() {
+    renderer.render(scene, camera);
+  }
+
+  init();
+  animate();
+});
