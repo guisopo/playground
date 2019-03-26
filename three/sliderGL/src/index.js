1463a519662d6aee0020f34e0d32e5dfcbfbaf8d
guisopo <solerguillermo@hotmail.com>
2019-03-26 20:49:47 +0100

    Changed TimelineMax

--- three/sliderGL/src/index.js
+++ three/sliderGL/src/index.js
@@ -1,23 +1,10 @@
 import "./main.scss";
 import { TimelineMax } from 'gsap';
 
-let state = {pos:0};
-
 document.body.addEventListener('click', () => {
   let tl = new TimelineMax();
-  let tl1 = new TimelineMax();
   tl
-    .to(state, 2, {
-      pos:1,
-      onUpdate: function() {
-        let percentage = `-${state.pos * 100}%`;
-        let percentage1 = `-${state.pos*state.pos * 100}%`;
-        let percentage2 = `-${state.pos*state.pos*state.pos * 100}%`;
-
-        tl1
-          .set('.slider__1 .slider__wrap', {x: percentage}, 0)
-          .set('.slider__2 .slider__wrap', {x: percentage1}, 0)
-          .set('.slider__3 .slider__wrap', {x: percentage2}, 0);
-      }
-    })
+    .to('.slider__1 .slider__wrap', 0, {x:'-100%'})
+    .to('.slider__2 .slider__wrap', 0.01, {x:'-100%'})
+    .to('.slider__3 .slider__wrap', 0.02, {x:'-100%'});
 });
