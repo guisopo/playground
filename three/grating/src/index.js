b14351ef4230ac51a6697f85a919b60becec9a6b
guisopo <solerguillermo@hotmail.com>
2019-04-07 11:04:04 +0200

    Created grating

--- three/grating/src/index.js
+++ three/grating/src/index.js
@@ -3,7 +3,8 @@ import * as THREE from 'three';
 const OrbitControls = require('three-orbit-controls')(THREE)
 
 let camera, scene, renderer;
-let geometry, material, mesh, controls;
+let geometry, material, meshX, meshY, controls;
+let size = 20;
 
 init();
 animate();
@@ -12,7 +13,7 @@ function init() {
   // SETUP
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+  camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 3000 );
   camera.position.z = 1;
 
   controls = new OrbitControls(camera);
@@ -22,7 +23,24 @@ function init() {
   renderer.setSize( window.innerWidth, window.innerHeight );
 
   // OBJECTS
+  let material = new THREE.LineBasicMaterial( { color: 0xffffff } );
   
+  for (let i = 0; i < size; i++) {
+    let geometryX = new THREE.Geometry();
+    let geometryY = new THREE.Geometry();
+    for (let j = 0; j < size; j++) {
+      geometryX.vertices.push(
+        new THREE.Vector3(i*0.1, j*0.1, 0)
+      );
+      geometryY.vertices.push(
+        new THREE.Vector3(j*0.1, i*0.1, 0)
+      );
+    }
+    meshX = new THREE.Line(geometryX, material);
+    meshY = new THREE.Line(geometryY, material);
+    scene.add(meshX);
+    scene.add(meshY);
+  }
 
   document.body.appendChild( renderer.domElement );
 }
