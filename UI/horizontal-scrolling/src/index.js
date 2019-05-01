5ef00e978145913cf7c25ab8ab7f145398b961f2
guisopo <solerguillermo@hotmail.com>
2019-05-01 07:28:36 +0200

    Clean up

--- UI/horizontal-scrolling/src/index.js
+++ UI/horizontal-scrolling/src/index.js
@@ -1,3 +1,38 @@
 import {TweenMax} from "gsap/TweenMax";
 var imagesLoaded = require('imagesloaded');
 import "./main.scss";
+
+class Smooth {
+  constructor() {
+    console.log(this);
+    this.bindAll();
+
+    this.el = document.querySelector('[data-scroll]');
+    this.content = [...document.querySelectorAll('[data-scroll-content]')];
+
+    this.dom = {
+      el: this.el,
+      content: this.content,
+      elements: [[...this.content[0].querySelectorAll('.js-slide')], [...this.content[1].querySelectorAll('.js-slide')]],
+    }
+  }
+
+  bindAll() {
+    ['scroll', 'run', 'resize']
+    .forEach( fn => this[fn] = this[fn].bind(this));
+  }
+
+  scroll() {
+
+  }
+
+  run() {
+
+  }
+
+  resize() {
+
+  }
+}
+
+const smooth = new Smooth;
