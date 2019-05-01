a6163770e5aed386557b2c1e9d0a2e562bb1fdfa
guisopo <solerguillermo@hotmail.com>
2019-05-01 10:38:32 +0200

    Improved wheel event setting bonduaries

--- UI/horizontal-scrolling/src/index.js
+++ UI/horizontal-scrolling/src/index.js
@@ -4,22 +4,52 @@ import "./main.scss";
 
 class Smooth {
   constructor() {
-    console.log(this);
     this.bindAll();
 
     this.el = document.querySelector('[data-scroll]');
-    this.content = [...document.querySelectorAll('[data-scroll-content]')];
+    this.content = document.querySelector('[data-scroll-content]');
 
     this.dom = {
       el: this.el,
       content: this.content,
-      elements: [[...this.content[0].querySelectorAll('.js-slide')], [...this.content[1].querySelectorAll('.js-slide')]],
+      elements: [...this.content.querySelectorAll('.js-slide')],
     }
+
+    this.data = {
+      total: this.dom.elements.length -1,
+      current: 0,
+      last: {}
+    }
+
+    this.bounds = {
+      elem: 0,
+      content: 0,
+      width: 0,
+      max: 0,
+      min: 0
+    }
+
+    this.rAF = null;
+
+    this.init();
   }
 
   bindAll() {
     ['scroll', 'run', 'resize']
     .forEach( fn => this[fn] = this[fn].bind(this));
+  }
+
+  preload() {
+    
+  }
+
+  setBounds(elements) {
+
+    elements.forEach((element, index) => {
+      
+      const bounds = element.getBoundingClientRect();
+
+    })
   }
 
   scroll() {
@@ -32,6 +62,44 @@ class Smooth {
 
   resize() {
 
+  }
+
+  addEvents() {
+    var moveX = 0;
+    window.addEventListener('wheel', (e) => {
+      const x  = e.deltaY;
+      moveX = moveX + x;
+      const width = this.content.getBoundingClientRect().width - window.innerWidth;
+      let delta = (this.content.getBoundingClientRect().width - window.innerWidth) - moveX;
+      if(delta > 0 && delta < width) {
+        this.content.style.transform = `translate3d(-${moveX}px, 0, 0)`;
+      } else if (delta < 0) {
+        moveX = width;
+        delta = 0;
+        this.content.style.transform = `translate3d(-${width}px, 0, 0)`;
+      } else if (delta > width) {
+        moveX = 0;
+        delta = width;
+        this.content.style.transform = `translate3d(0px, 0, 0)`;
+      }
+      console.log(moveX, delta, this.content.style.transform);
+    });
+  }
+
+  on() {
+    this.setBounds(this.dom.elements);
+    this.addEvents();
+
+    this.requestAnimationFrame();
+  }
+
+  requestAnimationFrame() {
+
+  }
+
+  init() {
+    this.preload();
+    this.on();
   }
 }
 
