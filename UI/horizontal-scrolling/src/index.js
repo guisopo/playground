b5c801e384527528dd80c6a1de2bcaea22792149
guisopo <solerguillermo@hotmail.com>
2019-05-06 19:12:37 +0200

    Add logical operator so either one of both deltas is being saved

--- UI/horizontal-scrolling/src/index.js
+++ UI/horizontal-scrolling/src/index.js
@@ -35,18 +35,36 @@ class Smooth {
     .forEach( fn => this[fn] = this[fn].bind(this));
   }
 
+  clamp(x, bound) {
+    return Math.min(Math.max(x, -bound), bound);
+  }
+
+  lerp (a, b, n) {
+    return (1 - n) * a + n * b
+  }
+
   wheel(e) {
-    this.x  = e.deltaY;
+    this.x  = e.deltaY || e.deltaX;
     this.moveX = this.moveX + this.x;
+    this.data.current = this.moveX;
   }
 
   run() {
-    this.skew = this.clamp(this.x, 500)/10;
-    this.scale = 1 - this.clamp(Math.abs(this.x), 1000)/1000;
-
     const width = this.content.getBoundingClientRect().width - window.innerWidth;
     let   delta = (this.content.getBoundingClientRect().width - window.innerWidth) - this.moveX;
-
+    
+    this.skew = this.clamp(this.x, 500)/10;
+    this.scale = 1 - this.clamp(Math.abs(this.x), 1000)/1000;
+    
+    this.data.last = this.lerp(this.data.last, this.data.current, 0.095);
+    this.data.last = Math.floor(this.data.last * 100) / 100;
+    
+    const diff = this.data.current - this.data.last;
+    const acc = Math.floor(diff / width * 100) / 100;
+    const velo =+ acc;
+    const bounce = 1 - Math.abs(velo * 0.25);
+    const skew = velo * 10;
+    
     if (delta < 0) {
       this.moveX = width;
       delta = 0;
@@ -55,32 +73,24 @@ class Smooth {
       delta = width;
     }
 
-    this.content.style.transform = `translate3d(-${this.moveX}px, 0, 0) 
-                                    skewX(${this.skew}deg)
-                                    scale(${this.scale})`;
+    this.content.style.transform = `translate3d(-${this.data.last }px, 0, 0) 
+                                    skewX(${skew}deg)
+                                    scale(${bounce})`;
 
     this.requestAnimationFrame();
   }
-
-  clamp(x, bound) {
-     return Math.min(Math.max(x, -bound), bound);
-  }
-
-  lerp (a, b, n) {
-		return (1 - n) * a + n * b
-	}
 
   addEvents() {
     window.addEventListener('wheel', this.wheel);
   }
-
+  
+  requestAnimationFrame() {
+    this.rAF = requestAnimationFrame(this.run);
+  }
+  
   on() {
     this.addEvents();
     this.requestAnimationFrame();
-  }
-
-  requestAnimationFrame() {
-    this.rAF = requestAnimationFrame(this.run);
   }
 
   init() {
