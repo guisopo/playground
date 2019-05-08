cb0a8dea5898fe686ad09dac80a1182f05e10ff2
guisopo <solerguillermo@hotmail.com>
2019-05-08 22:45:18 +0200

    Clean up scale

--- UI/horizontal-scrolling/src/index.js
+++ UI/horizontal-scrolling/src/index.js
@@ -53,14 +53,14 @@ class Smooth {
     const width = this.content.getBoundingClientRect().width - window.innerWidth;
     let   delta = (this.content.getBoundingClientRect().width - window.innerWidth) - this.moveX;
     
-    this.data.last = this.lerp(this.data.last, this.data.current, 0.075);
+    this.data.last = this.lerp(this.data.last, this.data.current, 0.095);
     this.data.last = Math.floor(this.data.last * 100) / 100;
     
     const diff = this.data.current - this.data.last;
     const acc = Math.floor(diff / width * 10000) / 10000;
     const velo =+ acc;
 
-    const scale = 1 - Math.abs(velo * 1);
+    const scale = 1 - Math.abs(velo);
     const skew = velo * 10;
     const rotate = velo * 20;
     
