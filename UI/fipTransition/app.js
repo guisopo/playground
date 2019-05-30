35f21c0e21f69eeb308d9fd2de1cc21eb64faad8
guisopo <solerguillermo@hotmail.com>
2019-05-30 16:21:24 +0200

    Make animation on click reusable and fixbug

--- UI/fipTransition/app.js
+++ UI/fipTransition/app.js
@@ -1,23 +1,19 @@
-const images = document.querySelectorAll('[data-scene="gallery"] figure');
 const app = document.querySelector('#app');
 
-// FIRST
-const smallImg1 = document.querySelector('[data-key="small-img-1"]');
-const loadedImage = document.querySelector('[data-key="small-img-1"] img');
-let firstRect;
-loadedImage.addEventListener('load', () => {
-  firstRect = smallImg1.getBoundingClientRect();
-});
-
-
 app.addEventListener('click', () => {
-  app.dataset.state =
-    app.dataset.state === 'detail' ? 'gallery' : 'detail';
+  let scene = document.querySelector(`[data-scene="${app.dataset.state}"]`);
   
+  // FIRST
+  const smallImg1 = scene.querySelector('[data-key="img-1"]');
+  let firstRect = smallImg1.getBoundingClientRect();
 
+  const state = (app.dataset.state = 
+                 app.dataset.state === 'detail'? 'gallery' : 'detail');
+  scene = document.querySelector(`[data-scene="${state}"]`);  
+  
   requestAnimationFrame(() => {
     // LAST
-    const bigImg1 = document.querySelector('[data-key="big-img-1"]');
+    const bigImg1 = scene.querySelector('[data-key="img-1"]');
     const lastRect = bigImg1.getBoundingClientRect();
 
     // INVERT
@@ -26,14 +22,14 @@ app.addEventListener('click', () => {
     const deltaW = firstRect.width / lastRect.width;
     const deltaH = firstRect.height / lastRect.height;
 
-    bigImg1.style.transform = 'none';
+    bigImg1.style.transition = 'none';
     bigImg1.style.transformOrigin = 'top left';
     bigImg1.style.transform = `translate(${deltaX}px, ${deltaY}px) scale(${deltaW}, ${deltaH})`;
+
     // PLAY
     requestAnimationFrame(() => {
       bigImg1.style.transition = "transform .5s cubic-bezier(.5,0,.5,1)";
       bigImg1.style.transform = "none";
     });
   });
-  
 });
