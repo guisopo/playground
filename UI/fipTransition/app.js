80abefc48bef5d076061a0801939616c66f5cc0a
guisopo <solerguillermo@hotmail.com>
2019-05-31 19:45:16 +0200

    Change naming variables

--- UI/fipTransition/app.js
+++ UI/fipTransition/app.js
@@ -4,16 +4,15 @@ app.addEventListener('click', () => {
   let scene = document.querySelector(`[data-scene="${app.dataset.state}"]`);
   
   // FIRST
-  const smallImg1 = scene.querySelector('[data-key="img-1"]');
-  let firstRect = smallImg1.getBoundingClientRect();
+  const firstImg = scene.querySelector('[data-key="img-1"]');
+  const firstRect = firstImg.getBoundingClientRect();
 
-  const state = (app.dataset.state = 
-                 app.dataset.state === 'detail'? 'gallery' : 'detail');
+  const state = (app.dataset.state = app.dataset.state === 'detail'? 'gallery' : 'detail');
   scene = document.querySelector(`[data-scene="${state}"]`);  
   
   requestAnimationFrame(() => {
     // LAST
-    const bigImg1 = scene.querySelector('[data-key="img-1"]');
+    const nextImg = scene.querySelector('[data-key="img-1"]');
     const lastRect = bigImg1.getBoundingClientRect();
 
     // INVERT
@@ -22,14 +21,14 @@ app.addEventListener('click', () => {
     const deltaW = firstRect.width / lastRect.width;
     const deltaH = firstRect.height / lastRect.height;
 
-    bigImg1.style.transition = 'none';
-    bigImg1.style.transformOrigin = 'top left';
-    bigImg1.style.transform = `translate(${deltaX}px, ${deltaY}px) scale(${deltaW}, ${deltaH})`;
+    nextImg.style.transition = 'none';
+    nextImg.style.transformOrigin = 'top left';
+    nextImg.style.transform = `translate(${deltaX}px, ${deltaY}px) scale(${deltaW}, ${deltaH})`;
 
     // PLAY
     requestAnimationFrame(() => {
-      bigImg1.style.transition = "transform .5s cubic-bezier(.5,0,.5,1)";
-      bigImg1.style.transform = "none";
+      nextImg.style.transition = "transform .3s cubic-bezier(.5,0,.5,1)";
+      nextImg.style.transform = "none";
     });
   });
 });
