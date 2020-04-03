e1ee02a51f0d2f37b1729565eb5322aa84cacd22
guisopo <solerguillermo@hotmail.com>
2020-04-03 11:29:19 +0200

    Restart project

--- UI/horizontal-scrolling/webpack.prod.js
+++ UI/horizontal-scrolling/webpack.prod.js
@@ -32,6 +32,10 @@ module.exports = merge(common, {
   module: {
     rules: [
       {
+        test:/\.css$/,
+        use: [ MiniCssExtractPlugin.loader, "css-loader" ]
+      },
+      {
         test:/\.scss$/,
         use: [ MiniCssExtractPlugin.loader, "css-loader", "sass-loader"  ]
       }
