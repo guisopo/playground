a5346e7db903c158956ffed96bacdb01b7643c71
guisopo <solerguillermo@hotmail.com>
2020-04-28 13:41:09 +0200

    Refactor code to a new class

--- pixi/fabio/src/index.js
+++ pixi/fabio/src/index.js
@@ -7,83 +7,103 @@ let img0 = require('./images/pic-1.jpeg');
 let img1 = require('./images/pic-2.jpeg');
 let img2 = require('./images/pic-3.jpeg');
 
-const nav = document.querySelector('.nav');
-let index = 0;
 
-[...nav.children].forEach(link => {
-  link.id = index;
-  index += 1;
-});
+class Window {
 
+  constructor() {
+    this.nav = document.querySelector('.nav');
 
-const app = new PIXI.Application({
-  width: window.innerWidth,
-  height: window.innerHeight
-});
+    this.app = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
 
-document.body.appendChild(app.view);
+    this.Filter = new PIXI.Filter(null, fragment);
+    this.loader = new PIXI.Loader();
+  }
 
-const loader = new PIXI.Loader();
+  bindAll() {
+    ['handleMouseOver']
+      .forEach( fn => this[fn] = this[fn].bind(this));
+  }
 
-loader.add('img0', img0);
-loader.add('img1', img1);
-loader.add('img2', img2);
+  addNavIds() {
+    let index = 0;
 
+    [...this.nav.children].forEach(link => {
+      link.id = index;
+      index += 1;
+    });
+  }
 
+  appendApp() {
+    document.body.appendChild(this.app.view);
+  }
 
-// load the texture we need
-loader.load((loader, resources) => {
+  addTextures() {
+    this.loader.add('img0', img0)
+               .add('img1', img1)
+               .add('img2', img2);
+  }
 
-  let Filter = new PIXI.Filter(null, fragment);
+  loadTextures() {
+    this.loader.load((loader, resources) => {
+
+      const bunny = new PIXI.Sprite(resources.img0.texture);
+    
+      // Calculate window and image Aspect Ratio
+      const winAspectRatio = window.innerWidth/window.innerHeight;
+      const imageAspectRatio = resources.img1.texture.width/resources.img1.texture.height;
   
+      // Create uniform with aspect ratio value
+      if(winAspectRatio > imageAspectRatio) {
+        this.Filter.uniforms.uvAspect = {x: 1., y: imageAspectRatio/winAspectRatio};
+      } else {
+        this.Filter.uniforms.uvAspect = {x: winAspectRatio/imageAspectRatio, y: 1.};
+      }
+  
+      bunny.filters = [this.Filter];
 
-  const bunny = new PIXI.Sprite(resources.img0.texture);
+      this.Filter.uniforms.uTextureOne = resources.img1.texture;
+      this.Filter.uniforms.uTextureTwo = resources.img2.texture;
+      this.Filter.uniforms.uProgress = 0.;
 
-    // Setup the position of the img
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
+      // Add the img to the scene we are building
+      this.app.stage.addChild(bunny);
+    });
+  }
 
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
+  handleMouseOver(e) {
+    const to = e.target.id;
 
-    // Calculate window and image Aspect Ratio
-    const winAspectRatio = window.innerWidth/window.innerHeight;
-    const imageAspectRatio = resources.img1.texture.width/resources.img1.texture.height;
-
-    // Create uniform with aspect ratio value
-    if(winAspectRatio > imageAspectRatio) {
-      Filter.uniforms.uvAspect = {x: 1., y: imageAspectRatio/winAspectRatio};
-    } else {
-      Filter.uniforms.uvAspect = {x: winAspectRatio/imageAspectRatio, y: 1.};
-    }
-
-    bunny.filters = [Filter];
-
-    Filter.uniforms.uTextureOne = resources.img1.texture;
-    Filter.uniforms.uTextureTwo = resources.img2.texture;
-    Filter.uniforms.uProgress = 0.;
-    // Add the img to the scene we are building
-    app.stage.addChild(bunny);
-
-    nav.addEventListener('mouseover', (e) => {
-      const to = e.target.id;
-
-      gsap.to(Filter.uniforms, {
-        duration: 0.75,
-        uProgress: to,
-        ease: 'Power3.easeOut',
-        onUpdate: () => {
-          let number = Math.floor(Filter.uniforms.uProgress);
-          Filter.uniforms.uTextureOne = resources[`img${number}`].texture;
-          if(number<2) {
-            Filter.uniforms.uTextureTwo = resources[`img${number+1}`].texture;
-          }
+    gsap.to(this.Filter.uniforms, {
+      duration: 0.75,
+      uProgress: to,
+      ease: 'Power3.easeOut',
+      onUpdate: () => {
+        let number = Math.floor(this.Filter.uniforms.uProgress);
+        
+        this.Filter.uniforms.uTextureOne = this.loader.resources[`img${number}`].texture;
+        if(number<2) {
+          this.Filter.uniforms.uTextureTwo = this.loader.resources[`img${number+1}`].texture;
         }
-      }); 
+      }
     });
+  }
 
-    // Listen for frame updates
-    app.ticker.add(() => {
-    });
-});
+  addEventListeners() {
+    this.nav.addEventListener('mouseover', this.handleMouseOver, true);
+  }
+
+  init() {
+    this.bindAll();
+    this.addNavIds();
+    this.appendApp();
+    this.addTextures();
+    this.loadTextures();
+    this.addEventListeners();
+  }
+}
+
+const a = new Window();
+a.init();
