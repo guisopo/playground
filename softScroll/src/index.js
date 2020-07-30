83b187eb9a3c2b3ce96a106657854c9b1e51be32
guisopo <solerguillermo@hotmail.com>
2020-07-30 15:33:03 +0200

    Fixed speed reverse direction issue

--- softScroll/src/index.js
+++ softScroll/src/index.js
@@ -10,23 +10,20 @@ document.addEventListener('wheel', (e) => {
 
 function raf() {
   position += speed;
-  speed *=0.7;
-
+  speed *=0.9;
+  (speed < 0.01 && speed > -0.01) ? speed = 0 : '';
+  
   let i = Math.round(position);
-  let diff = i - position;
-
+  
+  let diff = (i - position.toFixed(4));
+  
   position += diff * 0.03;
   position > 16 ? position = 0 : '';
   position < -0.25 ? position = 16 : '';
 
-  if(Math.abs(i - position) < 0.001) {
-    position = i;
-  }
-  console.log(i)
-  gsap.set('.dot', { y: position*50 });
+  gsap.set('.dot', { y: position * 50 });
   
   let currentSlide = Math.floor(position);
-  
   let nextSlide = ( currentSlide + 1 ) % 5;
 
   window.requestAnimationFrame(raf);
